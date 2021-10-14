836ceec1877d680bf5e86ffef53a87777e7367e6
Maxi Alem <maerc.maxi@gmail.com>
2021-10-14 02:21:08 -0300

    upgrade on search.js and customcommands shits

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -3,19 +3,13 @@ describe('Search elements', ()=>{//arrow function
         cy.visit('/');//sitio cargado en cypress.js como baseUrl
     })
     it('search for elements with multiple results', ()=>{
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('dress');
-            cy.get(index.searchButton).click();
-        })
+        cy.search("dress")//search es un custom command 
         cy.fixture('searchResults').then((searchResults)=>{//el nombre del fixture y el del then no necesariamente son iguales
             cy.get(searchResults.title).should('contain', 'dress');
         })
     })
     it('Search for elements with no results', ()=>{
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('qwerty');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('qwss')
         cy.fixture('searchResults').then((searchResults)=>{//el nombre del fixture y el del then no necesariamente son iguales
             cy.get(searchResults.alert).should('contain', 'No results were found for your search');
         })
